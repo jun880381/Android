0af1a7fbb64d8939de2022f959aa02bf01222fbb
jun880381 <144294898+jun880381@users.noreply.github.com>
2024-08-23 16:49:30 +0900

    Add files via upload

---
+++ Day3/MyApplicationProject/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplicationproject;
+
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.*;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private TextView textView;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = editText.getText().toString();
+                double result = (Math.pow(Integer.parseInt(str),2));
+                textView.setText(str + " 의 제곱 : " + String.valueOf(result));
+                editText.setText("");
+            }
+        });
+    }
+}
