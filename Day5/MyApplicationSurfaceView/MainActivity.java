1abc7d21d5b4ec019860f6639f453434ba675837
jun880381 <144294898+jun880381@users.noreply.github.com>
2024-08-27 16:05:35 +0900

    Add files via upload

---
+++ Day5/MyApplicationSurfaceView/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplicationserfaceview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //setContentView(R.layout.activity_main);
+
+        setContentView(new GameView(this));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
